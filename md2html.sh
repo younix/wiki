aa8e8060279e49731030673f1fc3b4a5600f9fe2
Jan Klemkow <j.klemkow@wemelug.de>
2023-10-03 13:18:48 +0200

    restructure page

--- md2html.sh
+++ md2html.sh
@@ -10,23 +10,29 @@ cat > $page <<-EOF
 	<html>
 	<head>
 		<title>${name}</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
 		<link rel="stylesheet" type="text/css" href="/style.css" />
 	</head>
 	<body>
-	<a href="/cgi-bin/edit?path=${name}">edit</a>
 EOF
 
-echo '<nav>' >> $page
-ls $(dirname $page)/*.html | while read file; do
+echo '<aside>' >> $page
+ls $(dirname $page)/*.html | sort | while read file; do
 	fname=$(basename ${file%.html})
 	cat >> $page <<-EOF
 		<a href="$(basename "${file}")">${fname}</a>
 	EOF
 done
-echo '</nav>' >> $page
+echo '</aside>' >> $page
 
+echo '<main>' >> $page
 cmark "${page%.html}.md" >> $page
+echo '</main>' >> $page
 
 cat >> $page <<-EOF
+	<hr>
+	<a href="/edit.cgi?path=${name}">edit</a>
+	</body>
 	</html>
 EOF
